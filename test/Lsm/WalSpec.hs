49804760e82f4e501d22e940868a544102d7c9cb
cl <cailue@apache.org>
2024-12-06 13:28:08 +0800

    test: multiple recover
    Signed-off-by: cl <cailue@apache.org>

--- test/Lsm/WalSpec.hs
+++ test/Lsm/WalSpec.hs
@@ -59,3 +59,41 @@ spec_wal = do
             (\_ -> (removeFile "test0.wal" `catch` (\(_ :: IOException) -> return ())))
             $ do
                 return ()
+    describe "Recover and Recover test" $ do
+        before (removeFile "test2.wal" `catch` (\(_ :: IOException) -> return ())) $ do
+            it "should recover a WAL file and continue writing" $ do
+                wal <- walCreate "test2.wal"
+                walWrite wal "key1" "value1"
+                walWrite wal "key2" "value2"
+                walWrite wal "key3" "old_value3"
+                walSync wal
+                _ <- hClose (walHandler wal)
+                result1 <- walRecover "test2.wal"
+                case result1 of
+                    Left err -> expectationFailure $ "Failed to recover WAL: " ++ show (err)
+                    Right (wal', pairs1) ->
+                        do
+                            Map.lookup "key1" pairs1 `shouldBe` Just "value1"
+                            Map.lookup "key2" pairs1 `shouldBe` Just "value2"
+                            Map.lookup "key3" pairs1 `shouldBe` Just "old_value3"
+                            -- Continue writing to the recovered WAL
+                            walWrite wal' "key3" "value3"
+                            walWrite wal' "key4" "value4"
+                            walWrite wal' "key5" "value5"
+                            walSync wal'
+                            _ <- hClose (walHandler wal')
+                            -- Recover again and check the results
+                            result2 <- walRecover "test2.wal"
+                            case result2 of
+                                Left err -> expectationFailure $ "Failed to recover WAL: " ++ show (err)
+                                Right (_, pairs2) ->
+                                    do
+                                        Map.lookup "key1" pairs2 `shouldBe` Just "value1"
+                                        Map.lookup "key2" pairs2 `shouldBe` Just "value2"
+                                        Map.lookup "key3" pairs2 `shouldBe` Just "value3"
+                                        Map.lookup "key4" pairs2 `shouldBe` Just "value4"
+                                        Map.lookup "key5" pairs2 `shouldBe` Just "value5"
+        after
+            (\_ -> (removeFile "test2.wal" `catch` (\(_ :: IOException) -> return ())))
+            $ do
+                return ()
