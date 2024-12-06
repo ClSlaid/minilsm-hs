e6660a2fbd3f50b01735015ebacbbe410e5ded5b
cl <cailue@apache.org>
2024-12-06 12:59:43 +0800

    patch: add more tests
    Signed-off-by: cl <cailue@apache.org>

--- test/Lsm/WalSpec.hs
+++ test/Lsm/WalSpec.hs
@@ -3,6 +3,7 @@
 module Lsm.WalSpec where
 
 import Control.Exception
+import qualified Data.ByteString.Lazy as BSL
 import Lsm.Wal
 import System.Directory (removeFile)
 import System.IO
@@ -26,5 +27,31 @@ spec_wal = do
                         do
                             pairs `shouldBe` [("key1", "value1"), ("key2", "value2"), ("key3", "value3")]
         after
-            (\_ -> (removeFile "test0.wal" `catch` (\(_ :: IOException) -> return ()))) $ do
-            return ()
+            (\_ -> (removeFile "test0.wal" `catch` (\(_ :: IOException) -> return ())))
+            $ do
+                return ()
+
+    describe "Corrupted WAL" $ do
+        before (removeFile "test1.wal" `catch` (\(_ :: IOException) -> return ())) $ do
+            it "recover a corrupted WAL file" $ do
+                wal <- walCreate "test1.wal"
+                walWrite wal "key1" "value1"
+                walWrite wal "key2" "value2"
+                walWrite wal "key3" "value3"
+                walSync wal
+                _ <- hClose (walHandler wal)
+
+                h <- openFile "test1.wal" WriteMode
+                hSeek h AbsoluteSeek 8
+                BSL.hPut h "V"
+                _ <- hClose h
+
+                result <- walRecover "test1.wal"
+                -- should fail
+                case result of
+                    Left _ -> do True `shouldBe` True
+                    Right _ -> expectationFailure $ "WAL recover should fail"
+        after
+            (\_ -> (removeFile "test0.wal" `catch` (\(_ :: IOException) -> return ())))
+            $ do
+                return ()
